4b801e210a263d457a5301a1146b21360a88218a
Yishin Li <ysli@araisrobo.com>
2016-02-26 11:17:01 +0800

    WIP: update listner.py for MDI integration

--- src/listener/scripts/listener.py
+++ src/listener/scripts/listener.py
@@ -39,10 +39,15 @@
 import rospy
 from std_msgs.msg import String
 from trajectory_msgs.msg import JointTrajectory
+from control_msgs.msg import FollowJointTrajectoryAction
+from control_msgs.msg import FollowJointTrajectoryActionGoal
 
 import os, time
-from machinekit import hal
 import struct
+
+from machinekit import hal
+import linuxcnc
+from linuxcnc_control import LinuxcncControl
 
 # print ring properties
 def print_ring(r):
@@ -59,31 +64,69 @@ msg_size = struct.calcsize(msg_fmt)
 
 # Global variable for ring handle
 w = 0
-
+pi2deg = 180.0 / 3.14159265359
+e = LinuxcncControl(1)
+e.g_raise_except = False
+if (e.ok_for_mdi() == False):
+    print "ERROR: not ready for MDI, quit ..."
+    quit()
+else:
+    print "Prepare for MDI ..."
+    e.prepare_for_mdi()
+print "ready to issue MDI commands ..."
 
 def callback(data):
     # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     print 'In callback'
-    for pose in data.points:
-        # print pose.positions[0]
-        print pose.time_from_start
-        print ["{0:0.2e}".format(i) for i in pose.positions]
+    global e
+    if (e.ok_for_mdi() == False):
+        print "ERROR: not ready for MDI, quit ..."
+        quit()
+    else:
+        print "Prepare for MDI ..."
+        e.prepare_for_mdi()
+    print "ready to issue MDI commands ..."
+    e.g("G21 F1500")
+    # print data.goal.trajectory.points.time_from_start.secs
+    # print data.goal.trajectory.points.time_from_start.nsecs
+    # print data.goal.trajectory.points[0].positions
+    prev_t = data.goal.trajectory.points[0].time_from_start.to_sec()
+    gcode_array = []
+    for point in data.goal.trajectory.points:
+        # print point.time_from_start
+        # print point.time_from_start.secs
+        # print point.time_from_start.nsecs
+        # print ["{0:0.2e}".format(i) for i in point.positions]
+        print "dt: %.5f" % float(point.time_from_start.to_sec() - prev_t)
+        global pi2deg
+        # print "G1 X%.5f Y%.5f Z%.5f A%.5f B%.5f C%.5f" % tuple([p * pi2deg for p in point.positions])
+        gcode = "G1 X%.5f Y%.5f Z%.5f A%.5f B%.5f C%.5f" % tuple([p * pi2deg for p in point.positions])
+        gcode_array.append(gcode)
+        prev_t = point.time_from_start.to_sec()
+        
+    for gcode in gcode_array:
+        print gcode
+        e.g(gcode)
 
-        if w.available > msg_size:
-            msg = struct.pack(msg_fmt, pose.time_from_start.secs,
-                                       pose.time_from_start.nsecs,
-                                       pose.positions[0],
-                                       pose.positions[1],
-                                       pose.positions[2],
-                                       pose.positions[3],
-                                       pose.positions[4],
-                                       pose.positions[5] )
-            w.write(msg)
+    # for pose in data.points:
+    #     # print pose.positions[0]
+    #     print pose.time_from_start
+    #     print ["{0:0.2e}".format(i) for i in pose.positions]
 
+    #     if w.available > msg_size:
+    #         msg = struct.pack(msg_fmt, pose.time_from_start.secs,
+    #                                    pose.time_from_start.nsecs,
+    #                                    pose.positions[0],
+    #                                    pose.positions[1],
+    #                                    pose.positions[2],
+    #                                    pose.positions[3],
+    #                                    pose.positions[4],
+    #                                    pose.positions[5] )
+    #         w.write(msg)
 
-    # investigate scratchpad region if one is defined
-    if w.scratchpad_size:
-        print "scratchpad:%d = '%s'" % (w.scratchpad_size, w.scratchpad.tobytes())
+    # # investigate scratchpad region if one is defined
+    # if w.scratchpad_size:
+    #     print "scratchpad:%d = '%s'" % (w.scratchpad_size, w.scratchpad.tobytes())
 
 
 def listener():
@@ -104,10 +147,13 @@ def listener():
         # see what we have
         print_ring(w)
 
-    rospy.Subscriber("/joint_path_command", JointTrajectory, callback)
+    # rospy.Subscriber("/joint_path_command", JointTrajectory, callback)
+    rospy.Subscriber("/rrbot/position_trajectory_controller/follow_joint_trajectory/goal", FollowJointTrajectoryActionGoal, callback)
+    print 'after Subscriber'
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+    print 'after spin'
 
 if __name__ == '__main__':
     print 'HelloThere'
